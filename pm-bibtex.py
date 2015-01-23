15f4991e83d5bc4fe7901feea2172e149ad099dc
chen <github@cyansite.cc>
2015-01-23 17:40:11 -0500

    almost done

--- pm-bibtex.py
+++ pm-bibtex.py
@@ -17,40 +17,88 @@ def pm_search(q, n):
 def pm_download(id_list):
     data = {'db':'pubmed', 'retmode':'xml', 'id':",".join([str(x) for x in id_list]), 'rettype':'medline'}
     res_html = urlopen(PM_DOWNLOAD, data=urlencode(data).encode('utf8')).read()
+    print(res_html.decode('utf8'))
     #return json.loads(res_html.decode('utf8'))
     tree = etree.fromstring(res_html)
-    authors = ""
-    authl = tree.xpath('/PubmedArticleSet/PubmedArticle/MedlineCitation/Article/AuthorList/Author/LastName')
-    authi = tree.xpath('/PubmedArticleSet/PubmedArticle/MedlineCitation/Article/AuthorList/Author/Initials')
-    authf = tree.xpath('/PubmedArticleSet/PubmedArticle/MedlineCitation/Article/AuthorList/Author/ForeName')    
-    for i in range(len(authl)):
-        lastname = authl[i].text
-        initials = ""
-        for j in range(len(authi[i].text)):
-            initials = initials + str(authi[i].text)[j]
-            initials = initials + "."
-        if i > 0:
-            authors = "%s and %s, %s" % (authors, lastname, initials)
-        else: #i = 0
-            authors = "%s, %s" % (lastname, initials)
+    bib_ref = []
+    for article_tree in tree.xpath('/PubmedArticleSet/PubmedArticle'):
+        authors = []
+        authl = article_tree.xpath('MedlineCitation/Article/AuthorList/Author/LastName')
+        authf = article_tree.xpath('MedlineCitation/Article/AuthorList/Author/ForeName')    
+        for lastname, firstname in zip(authl, authf):
+            authors.append('{}, {}'.format(lastname.text, firstname.text))
+        authors = " and ".join(authors)
 
-    return authors
+        title = article_tree.xpath('MedlineCitation/Article/ArticleTitle')[0].text
+        if title[-1] == '.':
+            title = title[:-1]
+        journal_tree = article_tree.xpath('MedlineCitation/Article/Journal')[0]
+        year = journal_tree.xpath('JournalIssue/PubDate/Year')[0].text
+        bibtexid = authl[0].text.lower() + year[-2:]
+        journal = journal_tree.xpath('Title')[0].text
+
+        volume = journal_tree.xpath('JournalIssue/Volume')[0].text
+
+        issue = journal_tree.xpath('JournalIssue/Issue')
+        if issue:
+            issue = issue[0].text
+        else:
+            issue = None
+        pages = article_tree.xpath('MedlineCitation/Article/Pagination/MedlinePgn')
+        pages = pages[0].text.replace('-','--')
+        pmid = article_tree.xpath('MedlineCitation/PMID')[0].text
+
+        idlist = article_tree.xpath('PubmedData/ArticleIdList/ArticleId')
+        doi = None
+        if idlist:
+            for id_node in idlist:
+                if id_node.attrib['IdType'] == 'doi':
+                    doi = id_node.text
+        keywords = [node.text for node in article_tree.xpath('MedlineCitation/KeywordList/Keyword')]
+        keywords = ",".join(keywords)
+
+        result = '''
+@article {{{},
+    author ={{{}}},
+    title = {{{}}},
+    year = {{{}}},
+    journal={{{}}},
+    volume={{{}}},'''.format(bibtexid, authors, title, year, journal, volume)
+        if issue:
+            result = '''{}
+    number={{{}}},'''.format(result, issue)
+        result = '''{}
+    pages={{{}}},
+    pmid={{{}}},'''.format(result, pages, pmid)
+        if doi:
+            result = '''{}
+    doi={{{}}},'''.format(result, doi)
+        result = '''{}
+    keywords={{{}}}
+}}'''.format(result, keywords)
+        
+        bib_ref.append(result)
+
+    return "\n".join(bib_ref)
 
 def loop(f):
     while True:
         q = input("query>")
+        if q == "bye":
+            break
         idlist = pm_search(q, n=1)
         if len(idlist) == 0:
             print('No matching result')
             continue
         else:
-            ref = pm_download(idlist)
-            
-            pprint(ref)
-
+            ref_text = pm_download(idlist)
+            f.write(ref_text)
+            f.flush()
+            print(ref_text)
 
 if __name__ == "__main__":
     import sys
-    print(pm_download(pm_search('amygdala', 1)))
-    #loop(None)
+    f = open(sys.argv[1], 'a')
+    loop(f)
+    f.close()
 
